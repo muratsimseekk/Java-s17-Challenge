994d0d9a3a030947ffeaab232903269744db5573
Murat Simsek <muratsimseekk@gmail.com>
2024-02-10 15:57:22 +0300

    project initialize

---
+++ src/test/java/com/workintechs17challenge/Challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintechs17challenge.Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
